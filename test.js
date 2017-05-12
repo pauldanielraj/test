5a49037a5f935385f9f2c25bf31f8dade09330b1
Paul Daniel <pauldaniel.raj@yahoo.com>
2017-05-12 15:58:20 -0400

    Initial commit

---
+++ test.js
@@ -0,0 +1,17 @@
+var fruit = "banana";
+
+switch (fruit) {
+  case "Apple":
+    alert("I love Apples");
+    break;
+  case "Grape":
+    alert("Im ok with Grapes");
+    break;
+  case "banana":
+    alert("i like it very much");
+    break;
+  default:
+    alert("Nothing")
+    break;
+
+}
