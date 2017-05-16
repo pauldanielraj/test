658925b0f61e0c321e44916a32e9a3c5baa5e2f4
Danny Paul <daniel@Dannys-MacBook-Pro.local>
2017-05-15 21:33:04 -0400

    update

--- scope.js
+++ scope.js
@@ -1,5 +1,5 @@
 function b(){
-  var myVar;
+  //var myVar;
   console.log(myVar);
 }
 
@@ -7,9 +7,10 @@ function a(){
   var myVar = 2;
   console.log(myVar);
   b();
+    
 }
 
-var myVar = 1;
-console.log(myVar);
+//var myVar = 1;
+//console.log(myVar);
 a();
-console.log(myVar);
+//console.log(myVar);
